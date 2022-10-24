b251c436fccba01493373e0738aca351226d0bd6
Kim Ferrante <kferrante187@gmail.com>
2022-10-24 11:27:35 -0400

    commit first message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-state-management';
+  title = "angular-state-management";
+
+  
 }
